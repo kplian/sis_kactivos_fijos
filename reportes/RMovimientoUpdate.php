473f070cdc4196d050d8f2deda334a4afa782b49
franklinespinozaalvarez <franklin.espinoza.alvarez@gmail.com>
2018-01-02 12:27:32 -0400

    modificando reporte A/T/D

--- reportes/RMovimientoUpdate.php
+++ reportes/RMovimientoUpdate.php
@@ -316,11 +316,11 @@ class RMovimientoUpdate extends  ReportePDF {
     function Firmas() {
         //<img  style="width: 110px; height: 110px;" src="' . $this->generarImagen($this->dataMaster[0]['responsable_depto'], 'RESPONSABLE ACTIVOS FIJOS') . '" alt="Logo">
         //<img  style="width: 110px; height: 110px;" src="' . $this->generarImagen($this->dataMaster[0]['responsable'], strtoupper($this->dataMaster[0]['nombre_cargo'])) . '" alt="Logo">
-        if ($this->getY()>=160 && $this->getY()<=234){
+        /*if ($this->getY()>=160 && $this->getY()<=234){
             $pos = $this->getY()+(234-$this->getY());
-            $this->setY($pos);
-        }
-
+            $this->setY(15);
+        }*/
+        $this->revisarfinPagina();
         $this->Ln(5);
 
         $tbl = '';
