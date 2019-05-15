936438932f64ac9162d14fc6c51f34490af734eb
Rodrigo Chumacero <rodrychm@gmail.com>
2019-05-15 09:38:30 -0400

    #5 correccion bugs

--- base/funciones/kaf.f_gestionar_cbte_depreciacion.sql
+++ base/funciones/kaf.f_gestionar_cbte_depreciacion.sql
@@ -10,7 +10,7 @@ $body$
 /****************************************************************************
 Autor: RAC KPLIAN
 Fecha:   03/05/2017
-Descripcion  Esta funcion gestiona los cbtes de depreciacon cuando son validados          
+Descripcion  Esta funcion gestiona los cbtes de depreciacon cuando son validados
 
 *****************************************************************************
 HISTORIAL DE MODIFICACIONES
@@ -19,6 +19,10 @@ HISTORIAL DE MODIFICACIONES
 Autor: RCM
 Fecha: 24/04/2018
 Descripción: Se modifica la función por la generación de 3 comprobantes para ETR
+
+***************************************************************************
+ ISSUE  SIS       EMPRESA       FECHA       AUTOR       DESCRIPCION
+ #5     KAF       ETR           11/02/2019  RCM         Se agrega código para setear fecha de última depreciación cuando pasa a estado finalizado
 *****************************************************************************/
 
 DECLARE
@@ -30,8 +34,8 @@ DECLARE
     va_id_tipo_estado           integer[];
     va_codigo_estado            varchar[];
     va_disparador               varchar[];
-    va_regla                    varchar[]; 
-    va_prioridad                integer[];    
+    va_regla                    varchar[];
+    va_prioridad                integer[];
     v_id_proceso_wf             integer;
     v_id_estado_wf              integer;
     v_codigo_estado             varchar;
@@ -47,9 +51,9 @@ DECLARE
 BEGIN
 
     v_nombre_funcion = 'kaf.f_gestionar_cbte_depreciacion';
- 
+
     --1) Obtención de datos
-    select 
+    select
     mov.id_movimiento,
     mov.id_estado_wf,
     mov.id_proceso_wf,
@@ -64,44 +68,44 @@ BEGIN
     mov.id_int_comprobante_4
     into
     v_registros
-    from  kaf.tmovimiento mov      
-    inner join wf.testado_wf ew 
+    from  kaf.tmovimiento mov
+    inner join wf.testado_wf ew
     on ew.id_estado_wf = mov.id_estado_wf
     inner join param.tcatalogo cat
     on cat.id_catalogo = mov.id_cat_movimiento
     where mov.id_int_comprobante = p_id_int_comprobante
     or mov.id_int_comprobante_aitb = p_id_int_comprobante
     or mov.id_int_comprobante_3 = p_id_int_comprobante
-    or mov.id_int_comprobante_4 = p_id_int_comprobante; 
-    
-    
+    or mov.id_int_comprobante_4 = p_id_int_comprobante;
+
+
     --2) Valida que el comprobante esté relacionado con un movimiento
     if v_registros.id_movimiento is null then
       raise exception 'El comprobante no está relacionado con ningún movimiento de activos fijos (id: %, %)',p_id_int_comprobante,v_registros;
     end if;
-    
-   
+
+
     --3) Verificación de validación de los 4 comprobantes
     select estado_reg
-    into v_estado_1 
+    into v_estado_1
     from conta.tint_comprobante
     where id_int_comprobante = v_registros.id_int_comprobante;
-    
+
     select estado_reg
     into v_estado_2
     from conta.tint_comprobante
     where id_int_comprobante = v_registros.id_int_comprobante_aitb;
-    
+
     select estado_reg
     into v_estado_3
     from conta.tint_comprobante
     where id_int_comprobante = v_registros.id_int_comprobante_3;
-    
+
     select estado_reg
     into v_estado_4
     from conta.tint_comprobante
     where id_int_comprobante = v_registros.id_int_comprobante_4;
-    
+
     if v_registros.movimiento = 'deprec' then
         if coalesce(v_estado_1,'')='validado' and coalesce(v_estado_2,'')='validado' and coalesce(v_estado_3,'')='validado' and coalesce(v_estado_4,'')='validado' then
             v_sw_fin = true;
@@ -113,14 +117,14 @@ BEGIN
     else
         raise exception 'El movimiento debe ser Depreciación o Actualización';
     end if;
-    
-    
+
+
      --4) Finaliza el movimiento si es que los 3 comprobantes de depreciación generados han sido validados
      if v_sw_fin then
         --------------------------------------------------------
         ---  Cambiar el estado del movimiento              -----
         --------------------------------------------------------
-        --Obtiene el siguiente estado del flujo 
+        --Obtiene el siguiente estado del flujo
         select
         *
         into
@@ -130,19 +134,19 @@ BEGIN
         va_regla,
         va_prioridad
         from wf.f_obtener_estado_wf(v_registros.id_proceso_wf, v_registros.id_estado_wf,null,'siguiente');
-              
+
         if va_codigo_estado[2] is not null then
           raise exception 'El proceso de WF esta mal parametrizado, sólo admite un estado siguiente para el estado: %', v_registros.estado;
         end if;
-                      
+
         if va_codigo_estado[1] is null then
-          raise exception 'El proceso de WF esta mal parametrizado, no se encuentra el estado siguiente, para el estado: %', v_registros.estado;           
+          raise exception 'El proceso de WF esta mal parametrizado, no se encuentra el estado siguiente, para el estado: %', v_registros.estado;
         end if;
-              
+
         --Estado siguiente
-        v_id_estado_actual = wf.f_registra_estado_wf(va_id_tipo_estado[1], 
-                                                    v_registros.id_funcionario, 
-                                                    v_registros.id_estado_wf, 
+        v_id_estado_actual = wf.f_registra_estado_wf(va_id_tipo_estado[1],
+                                                    v_registros.id_funcionario,
+                                                    v_registros.id_estado_wf,
                                                     v_registros.id_proceso_wf,
                                                     p_id_usuario,
                                                     p_id_usuario_ai, -- id_usuario_ai
@@ -151,22 +155,39 @@ BEGIN
                                                     'Comprobantes de depreciación validados');
 
         --Actualiza estado del proceso
-        update kaf.tmovimiento mov  set 
+        update kaf.tmovimiento mov  set
         id_estado_wf =  v_id_estado_actual,
         estado = va_codigo_estado[1],
         id_usuario_mod = p_id_usuario,
         fecha_mod = now(),
         id_usuario_ai = p_id_usuario_ai,
         usuario_ai = p_usuario_ai
-        where mov.id_movimiento = v_registros.id_movimiento; 
+        where mov.id_movimiento = v_registros.id_movimiento;
+
+        --#5 Inicio: Si el estado es finalizado, coloca la fecha de ultima depreciación a los AFV
+        if va_codigo_estado[1] = 'finalizado' then
+
+            --Actualiza la última fecha de depreciación
+            update kaf.tactivo_fijo_valores set
+            fecha_ult_dep = mov.fecha_hasta
+            from kaf.tmovimiento_af maf
+            inner join kaf.tmovimiento_af_dep mdep
+            on mdep.id_movimiento_af = maf.id_movimiento_af
+            inner join kaf.tmovimiento mov
+            on mov.id_movimiento = maf.id_movimiento
+            where maf.id_movimiento = v_registros.id_movimiento
+            and kaf.tactivo_fijo_valores.id_activo_fijo_valor = mdep.id_activo_fijo_valor;
+
+        end if;
+        --#5 Fin
 
     end if;
-    
+
     --Respuesta
     return true;
 
 EXCEPTION
-          
+
   WHEN OTHERS THEN
           v_resp='';
           v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
