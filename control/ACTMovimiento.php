e1fd346fef31f0e33b7b7866baf0ebfb5457ca9d
rchumacero <rodrychm@gmail.com>
2020-05-01 03:59:46 -0400

    #54 Creacion de AF solo cuando tenga importe mayor a cero

--- control/ACTMovimiento.php
+++ control/ACTMovimiento.php
@@ -11,6 +11,7 @@
          KAF     ETR         22-10-2015  RCM     Creación del archivo
  #39     KAF     ETR         22-11-2019  RCM     Reporte detalle depreciación, incluir traspasos
  #38     KAF     ETR         11-12-2019  RCM     Reingeniería importación de plantilla para movimientos especiales
+ #54     KAF     ETR         11-03-2020  RCM     Creación de AF solo cuando tenga importe mayor a cero
 **************************************************************************
 */
 require_once(dirname(__FILE__).'/../../pxp/pxpReport/ReportWriter.php');
@@ -409,7 +410,6 @@ class ACTMovimiento extends ACTbase{
 						$this->objParam->addParametro('codigo_af_rel', $fila['codigo_af_rel']);
 						$this->objParam->addParametro('codigo_af', $fila['codigo_activo']);
 
-
 						//Recorre todos los AFs del array y Guarda registro en movimiento af (kaf.tmovimiento9_af)
 						$cont_af = 1;
 						foreach ($cod_af_origen as $valor) {
@@ -419,37 +419,38 @@ class ACTMovimiento extends ACTbase{
 
 							//Si no existe registra en movimiento af
 							if(!$idMovimientoAf && trim($fila["activo_fijo_$cont_af"]) != '') {
-								//Registra sólo si tiene un importe
-								$this->objParam->addParametro('codigo_activo', $valor);
-								$this->objFunc = $this->create('MODMovimientoAf');
-	                    		$this->res = $this->objFunc->insertarMovimientoAfImp($this->objParam);
+								if($fila["activo_fijo_$cont_af"] > 0) { //#54
+									//Registra sólo si tiene un importe
+									$this->objParam->addParametro('codigo_activo', $valor);
+									$this->objFunc = $this->create('MODMovimientoAf');
+		                    		$this->res = $this->objFunc->insertarMovimientoAfImp($this->objParam);
 
-	                    		//Verifica si se produjo algún error
-								if ($this->res->getTipo() == 'ERROR') {
-			                    	$this->res->imprimirRespuesta($this->res->generarJson());
-						            exit;
-
-			                    } else {
-
-			                    	//Recupera el ID MOVIMIENTO AF resultado
-			                    	$dat = $this->res->getDatos();
-			                    	$idMovimientoAf = $dat['id_movimiento_af'];
-			                    	$this->objParam->addParametro('id_movimiento_af', $idMovimientoAf);
-			                    	$ids_movimiento_af[$idMovimientoAf] = $valor;
-
-			                    	//Guarda el detalle
-			                    	$this->objParam->addParametro('importe', $fila["activo_fijo_$cont_af"]);
-                    				$this->objFunc = $this->create('MODMovimientoAfEspecial');
-                    				$this->res = $this->objFunc->insertarMovimientoAfEspecialImportar($this->objParam);
-
-			                    	if ($this->res->getTipo() == 'ERROR') {
+		                    		//Verifica si se produjo algún error
+									if ($this->res->getTipo() == 'ERROR') {
 				                    	$this->res->imprimirRespuesta($this->res->generarJson());
 							            exit;
+
+				                    } else {
+
+				                    	//Recupera el ID MOVIMIENTO AF resultado
+				                    	$dat = $this->res->getDatos();
+				                    	$idMovimientoAf = $dat['id_movimiento_af'];
+				                    	$this->objParam->addParametro('id_movimiento_af', $idMovimientoAf);
+				                    	$ids_movimiento_af[$idMovimientoAf] = $valor;
+
+				                    	//Guarda el detalle
+				                    	$this->objParam->addParametro('importe', $fila["activo_fijo_$cont_af"]);
+	                    				$this->objFunc = $this->create('MODMovimientoAfEspecial');
+	                    				$this->res = $this->objFunc->insertarMovimientoAfEspecialImportar($this->objParam);
+
+				                    	if ($this->res->getTipo() == 'ERROR') {
+					                    	$this->res->imprimirRespuesta($this->res->generarJson());
+								            exit;
+					                    }
 				                    }
-			                    }
+								}//#54
 
 							} else if($idMovimientoAf && trim($fila["activo_fijo_$cont_af"]) != '') {
-
 								//Guarda el detalle
 		                    	$this->objParam->addParametro('importe', $fila["activo_fijo_$cont_af"]);
 		                    	$this->objParam->addParametro('id_movimiento_af', $idMovimientoAf); // #38
