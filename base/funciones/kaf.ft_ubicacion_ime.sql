97bcf157fc2ff6b3ed160627c8157827163f1ec2
rchumacero <rodrychm@gmail.com>
2020-05-04 23:26:42 -0400

    #64 inclusion de campo orden en la interfaz

--- base/funciones/kaf.ft_ubicacion_ime.sql
+++ base/funciones/kaf.ft_ubicacion_ime.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION "kaf"."ft_ubicacion_ime" (	
+CREATE OR REPLACE FUNCTION "kaf"."ft_ubicacion_ime" (
 				p_administrador integer, p_id_usuario integer, p_tabla character varying, p_transaccion character varying)
 RETURNS character varying AS
 $BODY$
@@ -9,13 +9,13 @@ $BODY$
  DESCRIPCION:   Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'kaf.tubicacion'
  AUTOR: 		 (admin)
  FECHA:	        15-06-2018 15:08:40
- COMENTARIOS:	
+ COMENTARIOS:
 ***************************************************************************
- HISTORIAL DE MODIFICACIONES:
-#ISSUE				FECHA				AUTOR				DESCRIPCION
- #0				15-06-2018 15:08:40								Funcion que gestiona las operaciones basicas (inserciones, modificaciones, eliminaciones de la tabla 'kaf.tubicacion'	
- #
- ***************************************************************************/
+ ISSUE  SIS       EMPRESA       FECHA       AUTOR       DESCRIPCION
+        KAF       ETR           15-06-2018  RCM         Creación del archivo
+ #64    KAF       ETR           05-05'2020  RCM         Agregar campo orden que ya está en la base de datos
+ ***************************************************************************
+ */
 
 DECLARE
 
@@ -26,21 +26,21 @@ DECLARE
 	v_nombre_funcion        text;
 	v_mensaje_error         text;
 	v_id_ubicacion	integer;
-			    
+
 BEGIN
 
     v_nombre_funcion = 'kaf.ft_ubicacion_ime';
     v_parametros = pxp.f_get_record(p_tabla);
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'SKA_UBIC_INS'
  	#DESCRIPCION:	Insercion de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		15-06-2018 15:08:40
 	***********************************/
 
 	if(p_transaccion='SKA_UBIC_INS')then
-					
+
         begin
         	--Sentencia de la insercion
         	insert into kaf.tubicacion(
@@ -52,7 +52,8 @@ BEGIN
 			fecha_reg,
 			usuario_ai,
 			id_usuario_mod,
-			fecha_mod
+			fecha_mod,
+			orden --#64
           	) values(
 			v_parametros.nombre,
 			'activo',
@@ -62,14 +63,12 @@ BEGIN
 			now(),
 			v_parametros._nombre_usuario_ai,
 			null,
-			null
-							
-			
-			
+			null,
+			v_parametros.orden --#64
 			)RETURNING id_ubicacion into v_id_ubicacion;
-			
+
 			--Definicion de la respuesta
-			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Locales almacenado(a) con exito (id_ubicacion'||v_id_ubicacion||')'); 
+			v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Locales almacenado(a) con exito (id_ubicacion'||v_id_ubicacion||')');
             v_resp = pxp.f_agrega_clave(v_resp,'id_ubicacion',v_id_ubicacion::varchar);
 
             --Devuelve la respuesta
@@ -77,10 +76,10 @@ BEGIN
 
 		end;
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'SKA_UBIC_MOD'
  	#DESCRIPCION:	Modificacion de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		15-06-2018 15:08:40
 	***********************************/
 
@@ -94,22 +93,23 @@ BEGIN
 			id_usuario_mod = p_id_usuario,
 			fecha_mod = now(),
 			id_usuario_ai = v_parametros._id_usuario_ai,
-			usuario_ai = v_parametros._nombre_usuario_ai
+			usuario_ai = v_parametros._nombre_usuario_ai,
+			orden = v_parametros.orden --#64
 			where id_ubicacion=v_parametros.id_ubicacion;
-               
+
 			--Definicion de la respuesta
-            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Locales modificado(a)'); 
+            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Locales modificado(a)');
             v_resp = pxp.f_agrega_clave(v_resp,'id_ubicacion',v_parametros.id_ubicacion::varchar);
-               
+
             --Devuelve la respuesta
             return v_resp;
-            
+
 		end;
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'SKA_UBIC_ELI'
  	#DESCRIPCION:	Eliminacion de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		15-06-2018 15:08:40
 	***********************************/
 
@@ -119,31 +119,31 @@ BEGIN
 			--Sentencia de la eliminacion
 			delete from kaf.tubicacion
             where id_ubicacion=v_parametros.id_ubicacion;
-               
+
             --Definicion de la respuesta
-            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Locales eliminado(a)'); 
+            v_resp = pxp.f_agrega_clave(v_resp,'mensaje','Locales eliminado(a)');
             v_resp = pxp.f_agrega_clave(v_resp,'id_ubicacion',v_parametros.id_ubicacion::varchar);
-              
+
             --Devuelve la respuesta
             return v_resp;
 
 		end;
-         
+
 	else
-     
+
     	raise exception 'Transaccion inexistente: %',p_transaccion;
 
 	end if;
 
 EXCEPTION
-				
+
 	WHEN OTHERS THEN
 		v_resp='';
 		v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
 		v_resp = pxp.f_agrega_clave(v_resp,'codigo_error',SQLSTATE);
 		v_resp = pxp.f_agrega_clave(v_resp,'procedimientos',v_nombre_funcion);
 		raise exception '%',v_resp;
-				        
+
 END;
 $BODY$
 LANGUAGE 'plpgsql' VOLATILE
--- base/funciones/kaf.ft_ubicacion_sel.sql
+++ base/funciones/kaf.ft_ubicacion_sel.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION "kaf"."ft_ubicacion_sel"(	
+CREATE OR REPLACE FUNCTION "kaf"."ft_ubicacion_sel"(
 				p_administrador integer, p_id_usuario integer, p_tabla character varying, p_transaccion character varying)
 RETURNS character varying AS
 $BODY$
@@ -8,13 +8,13 @@ $BODY$
  DESCRIPCION:   Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla 'kaf.tubicacion'
  AUTOR: 		 (admin)
  FECHA:	        15-06-2018 15:08:40
- COMENTARIOS:	
+ COMENTARIOS:
 ***************************************************************************
- HISTORIAL DE MODIFICACIONES:
-#ISSUE				FECHA				AUTOR				DESCRIPCION
- #0				15-06-2018 15:08:40								Funcion que devuelve conjuntos de registros de las consultas relacionadas con la tabla 'kaf.tubicacion'	
- #
- ***************************************************************************/
+ ISSUE  SIS       EMPRESA       FECHA       AUTOR       DESCRIPCION
+        KAF       ETR           15-06-2018  RCM         Creación del archivo
+ #64    KAF       ETR           05-05'2020  RCM         Agregar campo orden que ya está en la base de datos
+***************************************************************************
+*/
 
 DECLARE
 
@@ -22,21 +22,21 @@ DECLARE
 	v_parametros  		record;
 	v_nombre_funcion   	text;
 	v_resp				varchar;
-			    
+
 BEGIN
 
 	v_nombre_funcion = 'kaf.ft_ubicacion_sel';
     v_parametros = pxp.f_get_record(p_tabla);
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'SKA_UBIC_SEL'
  	#DESCRIPCION:	Consulta de datos
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		15-06-2018 15:08:40
 	***********************************/
 
 	if(p_transaccion='SKA_UBIC_SEL')then
-     				
+
     	begin
     		--Sentencia de la consulta
 			v_consulta:='select
@@ -51,25 +51,26 @@ BEGIN
 						ubic.id_usuario_mod,
 						ubic.fecha_mod,
 						usu1.cuenta as usr_reg,
-						usu2.cuenta as usr_mod	
+						usu2.cuenta as usr_mod,
+						ubic.orden --#64
 						from kaf.tubicacion ubic
 						inner join segu.tusuario usu1 on usu1.id_usuario = ubic.id_usuario_reg
 						left join segu.tusuario usu2 on usu2.id_usuario = ubic.id_usuario_mod
 				        where  ';
-			
+
 			--Definicion de la respuesta
 			v_consulta:=v_consulta||v_parametros.filtro;
 			v_consulta:=v_consulta||' order by ' ||v_parametros.ordenacion|| ' ' || v_parametros.dir_ordenacion || ' limit ' || v_parametros.cantidad || ' offset ' || v_parametros.puntero;
 
 			--Devuelve la respuesta
 			return v_consulta;
-						
+
 		end;
 
-	/*********************************    
+	/*********************************
  	#TRANSACCION:  'SKA_UBIC_CONT'
  	#DESCRIPCION:	Conteo de registros
- 	#AUTOR:		admin	
+ 	#AUTOR:		admin
  	#FECHA:		15-06-2018 15:08:40
 	***********************************/
 
@@ -82,23 +83,23 @@ BEGIN
 					    inner join segu.tusuario usu1 on usu1.id_usuario = ubic.id_usuario_reg
 						left join segu.tusuario usu2 on usu2.id_usuario = ubic.id_usuario_mod
 					    where ';
-			
-			--Definicion de la respuesta		    
+
+			--Definicion de la respuesta
 			v_consulta:=v_consulta||v_parametros.filtro;
 
 			--Devuelve la respuesta
 			return v_consulta;
 
 		end;
-					
+
 	else
-					     
+
 		raise exception 'Transaccion inexistente';
-					         
+
 	end if;
-					
+
 EXCEPTION
-					
+
 	WHEN OTHERS THEN
 			v_resp='';
 			v_resp = pxp.f_agrega_clave(v_resp,'mensaje',SQLERRM);
