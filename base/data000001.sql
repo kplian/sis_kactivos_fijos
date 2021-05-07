fe69fa0c1e5e92831614190697229a6d247e9eab
rchumacero <rodrychm@gmail.com>
2021-05-07 11:51:39 -0400

    #ETR-3901 actualizacion dep por baja

--- base/data000001.sql
+++ base/data000001.sql
@@ -1633,3 +1633,61 @@ AND id_moneda = 1
 AND id_movimiento = 495
 AND fecha = '01/03/2021';
 /***********************************F-DAT-RCM-KAF-ETR-3660-30/04/2021****************************************/
+
+/***********************************I-DAT-RCM-KAF-ETR-3901-07/05/2021****************************************/
+--REPORTE PROCESADO
+UPDATE kaf.treporte_detalle_dep2 AA SET
+depreciacion = 0,
+dep_abr = 0,
+depreciacion_acum = DD.depreciacion_acum_ant,
+monto_vigente = DD.monto_vigente_ant,
+total_dep = dep_ene + dep_feb + dep_mar,
+fecha_mod = now(),
+id_usuario_mod = 1
+FROM (
+	SELECT
+	rd.id, rd.id_movimiento, 
+	mdep.depreciacion_acum_ant, mdep.monto_vigente_ant
+	FROM kaf.treporte_detalle_dep2 rd
+	JOIN kaf.tactivo_fijo af ON af.codigo = rd.codigo
+	JOIN kaf.tactivo_fijo_valores afv ON afv.id_activo_fijo = af.id_activo_fijo 
+	JOIN kaf.tmovimiento_af_dep mdep ON mdep.id_activo_fijo_valor = afv.id_activo_fijo_valor 
+		AND mdep.id_moneda = rd.id_moneda 
+		AND date_trunc('month', mdep.fecha) = '01-04-2021'
+	WHERE rd.fecha = '01-04-2021'
+	AND rd.codigo IN (
+	'04.16.7.2209-0',
+	'04.16.7.2235-0',
+	'04.16.7.3251-0',
+	'04.16.7.3256-0',
+	'04.16.7.3257-0',
+	'04.16.7.3430-0',
+	'04.16.7.3466-0'
+	)
+) DD
+WHERE AA.id = DD.id;
+
+--KARDEX
+UPDATE kaf.tmovimiento_af_dep AA SET
+depreciacion = 0,
+depreciacion_acum = DD.depreciacion_acum_ant,
+depreciacion_per = DD.depreciacion_per_ant,
+monto_vigente = DD.monto_vigente_ant
+FROM (
+	SELECT 
+	af.codigo, afv.id_moneda, mdep.id_movimiento_af_dep,
+	0 AS depreciacion_ant, mdep.depreciacion_acum_ant , mdep.depreciacion_per_ant , mdep.monto_vigente_ant 
+	FROM kaf.tmovimiento_af_dep mdep
+	JOIN kaf.tactivo_fijo_valores afv ON afv.id_activo_fijo_valor = mdep.id_activo_fijo_valor 
+	JOIN kaf.tactivo_fijo af ON af.id_activo_fijo = afv.id_activo_fijo
+	WHERE af.codigo IN ('04.16.7.2209-0',
+	'04.16.7.2235-0',
+	'04.16.7.3251-0',
+	'04.16.7.3256-0',
+	'04.16.7.3257-0',
+	'04.16.7.3430-0',
+	'04.16.7.3466-0')
+	AND date_trunc('month', mdep.fecha) = '01-04-2021'
+) DD
+WHERE AA.id_movimiento_af_dep = DD.id_movimiento_af_dep;
+/***********************************F-DAT-RCM-KAF-ETR-3901-07/05/2021****************************************/
